f96ae9e7d0115128556d3f41c9cb60abd8512821
Alex Junior Pereira <alexjp17@gmail.com>
2021-02-05 20:07:56 -0300

    feito paginas de testes para home e login

--- src/pages/principais/home.tsx
+++ src/pages/principais/home.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import textStyle from '../../styles/text'
 
 export default function Home(){
+    const navigation = useNavigation()
+
+    function navigateToLogin(){
+        navigation.navigate('login')
+    }
+
     return (
-        <View/>
+        <View>
+            <Text style={textStyle.tests}>Home</Text>
+            <Button onPress={navigateToLogin} title="Ir para login"></Button>
+        </View>
     )
 }
--- src/pages/principais/login.tsx
+++ src/pages/principais/login.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import textStyle from '../../styles/text'
 
 export default function Login(){
+    const navigation = useNavigation()
+
+    function navigateToHome(){
+        navigation.navigate('home')
+    }
+
     return (
-        <View/>
+        <View>
+            <Text style={textStyle.tests}>Login</Text>
+            <Button onPress={navigateToHome} title="Ir para home"></Button>
+        </View>
     )
 }
+
