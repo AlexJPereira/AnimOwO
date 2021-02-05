e56ea4ef9c4a2abf92f9dfcbd53645535d98ce6c
Alex Junior Pereira <alexjp17@gmail.com>
2021-02-05 19:48:32 -0300

    adicionado rotas e esqueleto das paginas

---
+++ src/pages/rotas.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+// paginas
+import Login from './principais/login'
+import Home from './principais/home'
+import Perfil from './principais/perfil'
+import AnimePage from './principais/anime-page'
+
+import Configuracoes from './mais/configuracoes'
+import Downloads from './mais/downloads'
+import ListaCompleta from './mais/lista-completa'
+import Sobre from './mais/sobre'
+
+import Assistindo from './listas/assistindo'
+import Favoritos from './listas/favoritos'
+import PlanoAssistir from './listas/plano-assistir'
+
+const AppStack = createStackNavigator()
+
+export default function Rotas(){
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+                <AppStack.Screen name="login" component={Login}/>
+                <AppStack.Screen name="home" component={Home}/>
+                <AppStack.Screen name="perfil" component={Perfil}/>
+                <AppStack.Screen name="anime-page" component={AnimePage}/>
+                <AppStack.Screen name="configuracoes" component={Configuracoes}/>
+                <AppStack.Screen name="downloads" component={Downloads}/>
+                <AppStack.Screen name="lista-completa" component={ListaCompleta}/>
+                <AppStack.Screen name="sobre" component={Sobre}/>
+                <AppStack.Screen name="assistindo" component={Assistindo}/>
+                <AppStack.Screen name="favoritos" component={Favoritos}/>
+                <AppStack.Screen name="plano-assistir" component={PlanoAssistir}/>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    )
+}
