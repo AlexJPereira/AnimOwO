435fee47c5828eb0faa1e22ba3d1c16890f266f8
Leonardo Melo <leonardohenriquedemelo@outlook.com>
2021-02-06 14:29:02 -0300

    Criação da barra de tabs.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+
+//Page imports 
+import Home from './src/pages/home'; 
+import Watching from './src/pages/watching'; 
+import Plan from './src/pages/plan'; 
+import Favorite from './src/pages/favorite'; 
+
+const Tab = createBottomTabNavigator()
+
+interface barProps {
+  color: string; 
+  size: number; 
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={'Home'}
+        tabBarOptions={{
+          showLabel: false,
+          activeTintColor: '#AB3962',
+          inactiveTintColor: '#ffffff',
+          activeBackgroundColor: '#252121',
+          inactiveBackgroundColor: '#252121',
+          style: {
+                backgroundColor: '#252121',
+                paddingBottom: 3
+          }
+        }}
+    
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size} : barProps) => (
+              <Ionicons name="home-sharp" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Watching"
+          component={Watching}
+          options={{
+            tabBarIcon: ({color, size} : barProps) => (
+              <Ionicons name="tv" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Plan"
+          component={Plan}
+          options={{
+            tabBarIcon: ({color, size} : barProps) => (
+              <Ionicons name="list-circle" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={Favorite}
+          options={{
+            tabBarIcon: ({color, size} : barProps) => (
+              <Ionicons name="md-heart" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/favorite.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native'
+
+export default function Favorite() {
+    return(
+        <ScrollView>
+            <Text>Supose that's my favorite's tab.</Text>
+        </ScrollView>
+    )
+}
