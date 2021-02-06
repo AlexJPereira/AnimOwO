9ed4f3727f186ae1a1d5ff22e1da3ae781b8162a
Alex Junior Pereira <alexjp17@gmail.com>
2021-02-06 15:43:42 -0300

    removido botões de test das paginas de login e home

--- src/pages/principais/home.tsx
+++ src/pages/principais/home.tsx
@@ -1,21 +1,13 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text } from 'react-native'
 
 import textStyle from '../../styles/text'
-import { pagesNames } from '../rotas'
 
 export default function Home(){
-    const navigation = useNavigation()
-
-    function navigateToLogin(){
-        navigation.navigate(pagesNames.login)
-    }
-
     return (
         <View>
             <Text style={textStyle.tests}>Home</Text>
-            <Button onPress={navigateToLogin} title="Ir para login"></Button>
+            <Text>O Naruto pode ser um pouco duro </Text>
         </View>
     )
 }
--- src/pages/principais/login.tsx
+++ src/pages/principais/login.tsx
@@ -1,21 +1,12 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text } from 'react-native'
 
 import textStyle from '../../styles/text'
-import { pagesNames } from '../rotas'
 
 export default function Login(){
-    const navigation = useNavigation()
-
-    function navigateToHome(){
-        navigation.navigate(pagesNames.home)
-    }
-
     return (
         <View>
             <Text style={textStyle.tests}>Login</Text>
-            <Button onPress={navigateToHome} title="Ir para home"></Button>
         </View>
     )
 }
