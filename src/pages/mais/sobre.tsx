beb5eee99edb2a3c24e707a81b31ee88ae0c8f21
Alex Junior Pereira <alexjp17@gmail.com>
2021-02-22 21:57:03 -0300

    criado o sistema de login e a pagina de sobre

--- src/pages/mais/sobre.tsx
+++ src/pages/mais/sobre.tsx
@@ -1,14 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { 
     View,
-    Text } from 'react-native'
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+
+const logo = require('../../app-assets/logo/Logo-Icone.png')
 
 export default function Sobre(){
+    const _handlePressButtonAsync = async (address:string) => {
+        let result = await WebBrowser.openBrowserAsync(address);
+      };
     return (
-        <View>
-            <Text> 
-                Trabalho desenvolvido por Wykke e Leonardo. 
+        <View style={PageStyle.viewStyle}>
+            <TouchableOpacity 
+                onPress={() => _handlePressButtonAsync('https://github.com/wykke/tcc')}>
+                <Image
+                    style={PageStyle.logoPrincipal}
+                    source={logo}
+                />
+            </TouchableOpacity>
+            <Text style={PageStyle.textStyle}> 
+                Autores:
             </Text>
+            <TouchableOpacity 
+                onPress={() => _handlePressButtonAsync('https://github.com/wykke')}
+                style={PageStyle.cardStyle}>
+                <Image 
+                    style={PageStyle.logo}
+                    source={{
+                        uri: 'https://avatars.githubusercontent.com/u/38235118?s=460&v=4',
+                    }}
+                />
+                <Text style={PageStyle.textStyle}> Alex Junior </Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+                onPress={() => _handlePressButtonAsync('https://github.com/Ceu152')}
+                style={PageStyle.cardStyle}>
+                <Image 
+                    style={PageStyle.logo}
+                    source={{
+                        uri: 'https://avatars.githubusercontent.com/u/43916660?s=460&v=4',
+                    }}
+                />
+                <Text style={PageStyle.textStyle}> Leonardo Melo </Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const PageStyle = StyleSheet.create({
+    viewStyle: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    textStyle: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: 20,
+        lineHeight: 40,
+        letterSpacing: 1,
+        color: '#FFFFFF'
+    },
+    logo: {
+        width: 66,
+        height: 58,
+    },
+    cardStyle: {
+        paddingTop: 10,
+        borderRadius: 7,
+        borderColor: '#AB3962',
+        borderStyle: 'solid',
+        borderWidth: 5,
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        textAlign: 'center'
+    }, 
+    logoPrincipal: {
+        transform: [{scaleX: 0.5}, {scaleY: 0.5}]
+    }
+})
--- src/pages/principais/login.tsx
+++ src/pages/principais/login.tsx
@@ -1,20 +1,33 @@
 import React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
-import { Image } from 'react-native'
+import { View, Text, StyleSheet, Image} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
+import MalApi from '../../services/mal-api'
+import { setMalApi, malApi } from '../../services/global'
+
 import textStyle from '../../styles/text'
-import pagesNames from '../pagesNames'
 import Button from '../../components/button'
 import Link from '../../components/link'
 
 const LogoCompleto = require('../../app-assets/logo/Logo-OWO-MAL.png')
 
 export default function Login(){
-    const navigation = useNavigation()
+    setMalApi(new MalApi())
+    const navigator = useNavigation()
 
     function navigateToHome(){
-        navigation.navigate(pagesNames.home)
+        navigator.navigate('home')
+    }
+
+    async function login(){
+        const auth = await malApi.login()
+        
+        if(auth && auth.type == "success"){
+            alert("Login feito com sucesso")
+            navigator.navigate('home')
+        }else{
+            alert("Falha no login")
+        }
     }
 
     return (
@@ -23,7 +36,7 @@ export default function Login(){
             <Text style={textStyle.principal}>
                 Deseja entrar no aplicativo com sua conta do My Anime List?
             </Text>
-            <Button onPress={navigateToHome} title="ENTRE"></Button>
+            <Button onPress={login} title="ENTRE"></Button>
             <Link onPress={navigateToHome} title="Continuar como convidado"></Link>
         </View>
     )
