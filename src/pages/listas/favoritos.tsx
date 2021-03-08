d3ab612bc61b316401272722072e05e547d9e415
Leonardo <leonardohenriquedemelo@outlook.com>
2021-03-07 21:05:43 -0300

    Adição da página de favoritos e da página de plano de assitir.

--- src/pages/listas/favoritos.tsx
+++ src/pages/listas/favoritos.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
 
 import ListaPadrao from '../../components/lista-padrao'
 import NavBar from '../../components/navBar'
 import AnimeCardDetails from '../../components/anime-card-details'
+import { malApi } from '../../services/global'
 
-const FMA = require('../../app-assets/animes-tests/fma.jpg')
-const Gintama = require('../../app-assets/animes-tests/gintama.jpg')
-const HunterXHunter = require('../../app-assets/animes-tests/hunterxhunter.jpg')
-const SteinsGate = require('../../app-assets/animes-tests/steinsgate.jpg')
 
 export default function Favorito(){
+
+    const [state, setState] = useState({
+        assitindo: [{
+            animeTitle: '', 
+            animePic: 'https://idealservis.com.br/portal/wp-content/uploads/2014/07/default-placeholder.png', 
+            episodesNumber: 0
+        }]
+    })
+
+    async function getUserList(){
+        const response = await malApi.getUserList('on_hold', 'anime_title')
+        if(response)
+            setState({
+                assitindo: response.data.map((element) => ({
+                    animeTitle: element.node.title,
+                    animePic: element.node.main_picture.medium,
+                    episodesNumber: element.node.num_episodes,
+                }))
+            })
+    }
+
+    useEffect(()=>{
+        getUserList()
+    }, [])
+
     return (
         <View>
             <NavBar/>
             <ListaPadrao name="Favoritos">
-                <AnimeCardDetails animeImage={HunterXHunter} animeName="Hunter X Hunter" details="Reassistido 3 vezes"/>
-                <AnimeCardDetails animeImage={Gintama} animeName="Gintama" details="Reassistido 2 vezes"/>
-                <AnimeCardDetails animeImage={FMA} animeName="Fullmetal Alchemist" details="Reassistido 0 vezes"/>
-                <AnimeCardDetails animeImage={SteinsGate} animeName="Steins Gate" details="Reassistido 1 vez"/>
+                {
+                    state.assitindo.map((element, index) => 
+                        <AnimeCardDetails key={index}
+                            animeName={element.animeTitle} 
+                            animeImage={{ uri: element.animePic }} 
+                            details={`${element.episodesNumber} Episódios`}/>
+                    )
+                }
             </ListaPadrao>
         </View>
     )
--- src/pages/listas/plano-assistir.tsx
+++ src/pages/listas/plano-assistir.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View } from 'react-native'
 
 import ListaPadrao from '../../components/lista-padrao'
 import NavBar from '../../components/navBar'
 import AnimeCardDetails from '../../components/anime-card-details'
+import { malApi } from '../../services/global'
 
 const FMA = require('../../app-assets/animes-tests/fma.jpg')
 const Gintama = require('../../app-assets/animes-tests/gintama.jpg')
@@ -11,14 +12,43 @@ const HunterXHunter = require('../../app-assets/animes-tests/hunterxhunter.jpg')
 const SteinsGate = require('../../app-assets/animes-tests/steinsgate.jpg')
 
 export default function PlanoAssistir(){
+
+    const [state, setState] = useState({
+        assitindo: [{
+            animeTitle: '', 
+            animePic: 'https://idealservis.com.br/portal/wp-content/uploads/2014/07/default-placeholder.png', 
+            episodesNumber: 0
+        }]
+    })
+
+    async function getUserList(){
+        const response = await malApi.getUserList('plan_to_watch', 'anime_title')
+        if(response)
+            setState({
+                assitindo: response.data.map((element) => ({
+                    animeTitle: element.node.title,
+                    animePic: element.node.main_picture.medium,
+                    episodesNumber: element.node.num_episodes,
+                }))
+            })
+    }
+
+    useEffect(()=>{
+        getUserList()
+    }, [])
+
     return (
         <View>
             <NavBar/>
             <ListaPadrao name="Plano de assistir">
-                <AnimeCardDetails animeImage={Gintama} animeName="Gintama" details="24 Episódios"/>
-                <AnimeCardDetails animeImage={HunterXHunter} animeName="Hunter X Hunter" details="12 Episódios"/>
-                <AnimeCardDetails animeImage={SteinsGate} animeName="Steins Gate" details="6 Episódios"/>
-                <AnimeCardDetails animeImage={FMA} animeName="Fullmetal Alchemist" details="20 Episódios"/>
+                {
+                    state.assitindo.map((element, index) => 
+                        <AnimeCardDetails key={index}
+                            animeName={element.animeTitle} 
+                            animeImage={{ uri: element.animePic }} 
+                            details={`${element.episodesNumber} Episódios`}/>
+                    )
+                }
             </ListaPadrao>
         </View>
     )
